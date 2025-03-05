bfbd928aed971552b185fedf1f772b6794e85911
Your Name <your@example.com>
2025-03-05 15:30:44 -0800

    changing layout of profile page

--- app/profile/_layout.js
+++ app/profile/_layout.js
@@ -1,13 +1,27 @@
 import { Stack } from 'expo-router';
+import React, { useContext } from 'react';
+import { ThemeContext } from '../_layout';
 
 export default function ProfileLayout() {
+  const { isDark } = useContext(ThemeContext);
+
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: isDark ? '#333' : '#FFF',
+        },
+        headerTintColor: isDark ? '#FFF' : '#333',
+        headerTitleStyle: {
+          fontWeight: '600',
+          fontSize: 20,
+        },
+      }}
+    >
       <Stack.Screen name="index" options={{ title: 'Profile' }} />
       <Stack.Screen name="account" options={{ title: 'Account' }} />
       <Stack.Screen name="createJournal" options={{ title: 'Create Journal' }} />
       <Stack.Screen name="[alljournalId]" options={{ title: 'All Journal Entries' }} />
-      <Stack.Screen name="[userId]" options={{ title: 'User Profile' }} />
     </Stack>
   );
 }
